2e4e83ebd4ac7dda1d5ec21d69b6e1fe79605341
gastonmatta16 <gaston.matta06@gmail.com>
2019-01-29 22:16:03 -0300

    First Excersice

--- topics/src/POO/ConnectionSingleton.java
+++ topics/src/POO/ConnectionSingleton.java
@@ -4,10 +4,33 @@ import java.sql.*;
 
 public class ConnectionSingleton {
 
-    private static Connection connection;
+    private static Connection connection = null;
     private String driver;
     private String url;
     private String user;
     private String password;
 
+    private ConnectionSingleton() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "user";
+        String password = "password";
+
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            new ConnectionSingleton();
+        }
+
+        return connection;  
+    }
 }
