f90ed715692f4280d9b289ae9546f9d194d54bb8
gastonmatta16 <gaston.matta06@gmail.com>
2019-01-29 22:08:23 -0300

    First Excersice

---
+++ topics/src/POO/ConnectionSingleton.java
@@ -0,0 +1,13 @@
+package POO;
+
+import java.sql.*;
+
+public class ConnectionSingleton {
+
+    private static Connection connection;
+    private String driver;
+    private String url;
+    private String user;
+    private String password;
+
+}
